5a3d0c7fd37ee95db930dc628f0fedeaa6f75749
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-08-11 19:44:06 -0400

    finished project

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,11 +4,18 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const { posts } = props;
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {posts.comments.map(fr => {
+        return <Comment 
+        key={fr.id}
+        comment={fr}
+        />
+      })
+    }
     </div>
   );
 };
